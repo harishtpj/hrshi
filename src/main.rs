519b1fb1b4ad4debbfb186c8991b57d137e7a395
harishtpj <harishtpj@hotmail.com>
2025-06-30 20:40:41 +0530

    Added option to quit app

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-use std::io::{self, Read};
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        match b {
-           Ok(b) => {
-               let c = b as char;
-               if c.is_control() {
-                   println!("Binary: {b:08b} ASCII: {b:#03} \r");
-               } else {
-                   println!("Binary: {b:08b} ASCII: {b:#03} Character: {c:#?}\r");
-               }
-               if c == 'q' {
-                   break;
-               }
-           } 
-           Err(e) => println!("Error: {e}")
-        }
-    }
-    disable_raw_mode().unwrap();
+    Editor::default().run();
 }
