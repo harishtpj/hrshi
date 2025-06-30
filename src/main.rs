30b3f0038aae7215d54ad25ffd3dab8fa41f930e
harishtpj <harishtpj@hotmail.com>
2025-06-30 15:24:44 +0530

    Added support for raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{self, Read};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        match b {
+           Ok(b) => {
+               let c = b as char;
+               if c.is_control() {
+                   println!("Binary: {b:08b} ASCII: {b:#03} \r");
+               } else {
+                   println!("Binary: {b:08b} ASCII: {b:#03} Character: {c:#?}\r");
+               }
+               if c == 'q' {
+                   break;
+               }
+           } 
+           Err(e) => println!("Error: {e}")
+        }
+    }
+    disable_raw_mode().unwrap();
 }
